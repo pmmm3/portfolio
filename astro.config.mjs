06011c82e4c8b6e24e11b2387016413cfa042d98
Pablo Moreno <pablo.triplem@gmail.com>
2024-01-18 15:31:31 +0100

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://pmmmm3.github.io",
+	base: "/",
 	integrations: [tailwind()],
 	i18n: {
 		locales: ["en", "es"],
