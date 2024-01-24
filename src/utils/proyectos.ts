0e8e112ae12ab9fa3bb527af71cc709e7048838e
Pablo Moreno <pablo.triplem@gmail.com>
2024-01-24 01:06:18 +0100

    V1 - Projects

--- src/utils/proyectos.ts
+++ src/utils/proyectos.ts
@@ -9,4 +9,59 @@ export const PROJECTS = [{
     alt:""
 
   },
+  {
+    title: "PsicoSalud",
+    description:
+      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
+    link: "https://svgl.vercel.app/",
+    github: "https://github.com/pheralb/svgl",
+    video: "E0rLh-8WelU",
+    image: "https://images.pexels.com/photos/1089842/pexels-photo-1089842.jpeg?auto=compress&cs=tinysrgb&w=600",
+    alt:""
+
+  },
+  {
+    title: "PsicoSalud",
+    description:
+      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
+    link: "https://svgl.vercel.app/",
+    github: "https://github.com/pheralb/svgl",
+    video: "E0rLh-8WelU",
+    image: "https://images.pexels.com/photos/1089842/pexels-photo-1089842.jpeg?auto=compress&cs=tinysrgb&w=600",
+    alt:""
+
+  },
+  {
+    title: "PsicoSalud",
+    description:
+      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
+    link: "https://svgl.vercel.app/",
+    github: "https://github.com/pheralb/svgl",
+    video: "E0rLh-8WelU",
+    image: "",
+    alt:""
+
+  },
+  {
+    title: "PsicoSalud",
+    description:
+      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
+    link: "https://svgl.vercel.app/",
+    github: "https://github.com/pheralb/svgl",
+    video: "E0rLh-8WelU",
+    image: "",
+    alt:""
+
+  },
+  {
+    title: "PsicoSalud",
+    description:
+      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
+    link: "https://svgl.vercel.app/",
+    github: "https://github.com/pheralb/svgl",
+    video: "E0rLh-8WelU",
+    image: "",
+    alt:""
+
+  },
 ];
