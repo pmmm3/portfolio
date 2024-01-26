c4b0a0131a24c6c83d00a062c7d2e9cb7be64047
Pablo Moreno <pablo.triplem@gmail.com>
2024-01-26 01:10:29 +0100

    Agregar Proyectos

--- src/utils/proyectos.ts
+++ src/utils/proyectos.ts
@@ -1,66 +1,55 @@
-export const PROJECTS = [{
+export const PROJECTS = [
+  {
+    title: "⚠️​ En desarrollo ⚠️​",
+    description:
+      "Sistema basado en (LLM). Facilitará la interpretación de eventos de seguridad producidos por un IDS, reduciendo tiempos de respuesta e impacto de los ataques.",
+    link: "",
+    github: "",
+    video: "",
+    image: "https://nordicapis.com/wp-content/uploads/7-Large-Language-Model-LLM-APIs.png",
+    alt:""
+
+  },{
     title: "PsicoSalud",
     description:
       "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
-    link: "https://svgl.vercel.app/",
-    github: "https://github.com/pheralb/svgl",
+    link: "",
+    github: "https://github.com/pmmm3/tfg_pwa",
     video: "E0rLh-8WelU",
     image: "",
     alt:""
 
   },
   {
-    title: "PsicoSalud",
+    title: "Buscador Custom",
     description:
-      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
-    link: "https://svgl.vercel.app/",
-    github: "https://github.com/pheralb/svgl",
-    video: "E0rLh-8WelU",
-    image: "https://images.pexels.com/photos/1089842/pexels-photo-1089842.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "Creación de 0 de un buscador que indexa datos de Scopus, crea facetas e indices con Lucene en Java",
+    link: "",
+    github: "https://github.com/pmmm3/Universidad/tree/main/Cuarto/RI",
+    video: "",
+    image: "https://logowik.com/content/uploads/images/apache-lucene9001.logowik.com.webp",
     alt:""
 
   },
   {
-    title: "PsicoSalud",
+    title: "Monopoly",
     description:
-      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
-    link: "https://svgl.vercel.app/",
-    github: "https://github.com/pheralb/svgl",
-    video: "E0rLh-8WelU",
-    image: "https://images.pexels.com/photos/1089842/pexels-photo-1089842.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "Aplicación con Java que simula el juego de mesa Monopoly",
+    link: "",
+    github: "https://github.com/pmmm3/Universidad/tree/main/Segundo/Monopoly/civitas_java%20(1)/proyecto_civitas",
+    video: "",
+    image: "https://images.pexels.com/photos/19590482/pexels-photo-19590482/free-photo-of-monopoly-board-game.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     alt:""
 
   },
   {
-    title: "PsicoSalud",
+    title: "Weather App",
     description:
-      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
-    link: "https://svgl.vercel.app/",
-    github: "https://github.com/pheralb/svgl",
-    video: "E0rLh-8WelU",
-    image: "",
-    alt:""
-
-  },
-  {
-    title: "PsicoSalud",
-    description:
-      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
-    link: "https://svgl.vercel.app/",
-    github: "https://github.com/pheralb/svgl",
-    video: "E0rLh-8WelU",
-    image: "",
-    alt:""
-
-  },
-  {
-    title: "PsicoSalud",
-    description:
-      "Aplicación web y móvil que permite la administración de cuestionarios psiquiátricos en pacientes de atención primaria, con el propósito principal de evaluar neurosis y archivar los cuestionarios y la información del paciente para futuros seguimientos.",
-    link: "https://svgl.vercel.app/",
-    github: "https://github.com/pheralb/svgl",
-    video: "E0rLh-8WelU",
-    image: "",
+      "Aplicación web con Angular y Pyhton que muestra el tiempo de una ciudad, con la posibilidad de añadir ciudades a favoritos y ver el tiempo de las mismas",
+    link: "",
+    github: "https://github.com/pmmm3/WeatherFront",
+    video: "",
+    image: "https://images.pexels.com/photos/267149/pexels-photo-267149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     alt:""
 
   },
